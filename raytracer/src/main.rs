89ad4d84b6eeaa619b831ce7a8544d031e1aba5b
time-interspace <1529558183@qq.com>
2023-07-04 17:48:52 +0800

    book1-image4

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -16,9 +16,9 @@ fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     let c = dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
     if discriminant < 0.0 {
-        return -1.0;
+        -1.0
     } else {
-        return (-b - discriminant.sqrt()) / (2.0 * a);
+        (-b - discriminant.sqrt()) / (2.0 * a)
     }
 }
 fn ray_color(r: Ray) -> Color {
