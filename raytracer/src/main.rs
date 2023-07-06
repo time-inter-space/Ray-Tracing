61101ef440c1adc47390cd1081a9bf652177ae78
time-interspace <1529558183@qq.com>
2023-07-06 14:22:22 +0800

    book1-image6

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -14,7 +14,10 @@ mod hittable_list;
 use hittable_list::*;
 
 mod rtweekend;
-//use rtweekend::*;
+use rtweekend::*;
+
+mod camera;
+use camera::*;
 
 use console::style;
 use image::{ImageBuffer, RgbImage};
@@ -33,13 +36,14 @@ fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
 }
 
 fn main() {
-    let path = std::path::Path::new("output/book1/image5.jpg");
+    let path = std::path::Path::new("output/book1/image6.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = ((image_width as f64) / aspect_ratio) as u32;
+    let samples_per_pixel = 100;
     let quality = 100;
     let mut img: RgbImage = ImageBuffer::new(image_width, image_height);
 
@@ -47,15 +51,7 @@ fn main() {
     world.add(Rc::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Rc::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let cam = Camera::new();
 
     let progress = if option_env!("CI").unwrap_or_default() == "true" {
         ProgressBar::hidden()
@@ -67,7 +63,7 @@ fn main() {
         for i in 0..image_width {
             let pixel = img.get_pixel_mut(i, image_height - j - 1);
 
-            let u = (i as f64) / ((image_width - 1) as f64);
+            /*let u = (i as f64) / ((image_width - 1) as f64);
             let v = (j as f64) / ((image_height - 1) as f64);
             let r = Ray {
                 orig: origin,
@@ -78,9 +74,33 @@ fn main() {
                 (255.999 * pixel_color.e0) as u8,
                 (255.999 * pixel_color.e1) as u8,
                 (255.999 * pixel_color.e2) as u8,
+            ]);*/
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            let mut s = 0;
+            loop {
+                if s >= samples_per_pixel {
+                    break;
+                }
+                let u = ((i as f64) + random_double()) / ((image_width - 1) as f64);
+                let v = ((j as f64) + random_double()) / ((image_height - 1) as f64);
+                let r = cam.get_ray(u, v);
+                pixel_color = pixel_color + ray_color(&r, &world);
+                s += 1;
+            }
+            let mut r = pixel_color.e0;
+            let mut g = pixel_color.e1;
+            let mut b = pixel_color.e2;
+            let scale = 1.0 / (samples_per_pixel as f64);
+            r *= scale;
+            g *= scale;
+            b *= scale;
+            *pixel = image::Rgb([
+                (256.0 * clamp(r, 0.0, 0.999)) as u8,
+                (256.0 * clamp(g, 0.0, 0.999)) as u8,
+                (256.0 * clamp(b, 0.0, 0.999)) as u8,
             ]);
+            progress.inc(1);
         }
-        progress.inc(1);
     }
     progress.finish();
 
--- raytracer/src/rtweekend.rs
+++ raytracer/src/rtweekend.rs
@@ -1,5 +1,22 @@
+use rand::Rng;
+
 /*pub const PI: f64 = 3.1415926535897932385;
 
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * PI / 180.0
 }*/
+
+pub fn random_double() -> f64 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0.0..1.0)
+}
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min {
+        return min;
+    }
+    if x > max {
+        return max;
+    }
+    x
+}
