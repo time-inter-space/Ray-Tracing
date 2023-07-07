9d6c35650548e12c20767212e8983956c50062ff
time-interspace <1529558183@qq.com>
2023-07-07 10:07:01 +0800

    book1-image17

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -68,8 +68,9 @@ fn main() {
     let quality = 100;
     let mut img: RgbImage = ImageBuffer::new(image_width, image_height);
 
-    let r = (PI / 4.0).cos();
     let mut world = HittableList::new();
+
+    let r = (PI / 4.0).cos();
 
     let material_left = Rc::new(Lambertian::new(Color::new(0.0, 0.0, 1.0)));
     let material_right = Rc::new(Lambertian::new(Color::new(1.0, 0.0, 0.0)));
@@ -84,8 +85,46 @@ fn main() {
         r,
         material_right,
     )));
-
     let cam = Camera::new(90.0, aspect_ratio);
+
+    /*let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Dielectric::new(1.5));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
+
+    world.add(Rc::new(Sphere::new(
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.0),
+        0.5,
+        material_center,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
+        material_left.clone(),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.45,
+        material_left,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
+        material_right,
+    )));
+
+    let cam = Camera::new(
+        Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        90.0,
+        aspect_ratio,
+    );*/
 
     let progress = if option_env!("CI").unwrap_or_default() == "true" {
         ProgressBar::hidden()
--- raytracer/src/rtweekend.rs
+++ raytracer/src/rtweekend.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 
-pub const PI: f64 = 3.1415926535897932385;
+pub const PI: f64 = 3.141_592_653_589_793;
 
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * PI / 180.0
