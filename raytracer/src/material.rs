3a99ebd4de6248e0f0ac10cbc80cb2e671988f77
time-interspace <1529558183@qq.com>
2023-07-07 09:22:46 +0800

    book1-image16

--- raytracer/src/material.rs
+++ raytracer/src/material.rs
@@ -63,6 +63,11 @@ impl Dielectric {
         Dielectric { ir }
     }
 }
+fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+    let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    r0 *= r0;
+    r0 + (1.0 - r0) * (1.0 - cosine).powf(5.0)
+}
 impl Material for Dielectric {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<Pair<Color, Ray>> {
         let attenuation = Color::new(1.0, 1.0, 1.0);
@@ -72,17 +77,15 @@ impl Material for Dielectric {
             self.ir
         };
         let unit_direction = unit_vector(r_in.direction());
-        let mut cos_theta = dot(-unit_direction, rec.normal);
-        if cos_theta > 1.0 {
-            cos_theta = 1.0;
-        }
+        let cos_theta = dot(-unit_direction, rec.normal).min(1.0);
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        let direction = if cannot_refract {
-            reflect(unit_direction, rec.normal)
-        } else {
-            refract(unit_direction, rec.normal, refraction_ratio)
-        };
+        let direction =
+            if cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double() {
+                reflect(unit_direction, rec.normal)
+            } else {
+                refract(unit_direction, rec.normal, refraction_ratio)
+            };
         let scattered = Ray::new(rec.p, direction);
         Some(Pair::new(attenuation, scattered))
     }
