a70f653dc7a5a5460592ad36d8835437db690d10
time-interspace <1529558183@qq.com>
2023-07-11 17:29:15 +0800

    book2-image13

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -196,7 +196,7 @@ fn two_perlin_spheres() -> HittableList {
 }*/
 
 fn main() {
-    let path = std::path::Path::new("output/book2/image12.jpg");
+    let path = std::path::Path::new("output/book2/image13.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
