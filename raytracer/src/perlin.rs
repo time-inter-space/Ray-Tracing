54083b19918a84479372d714f6afec60ee17e950
time-interspace <1529558183@qq.com>
2023-07-11 11:01:50 +0800

    book2-image11

--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -2,45 +2,41 @@ use crate::*;
 
 const POINT_COUNT: i32 = 256;
 pub struct Perlin {
-    ranfloat: Vec<f64>,
+    ranvec: Vec<Vec3>,
     perm_x: Vec<i32>,
     perm_y: Vec<i32>,
     perm_z: Vec<i32>,
 }
 impl Perlin {
     pub fn new() -> Perlin {
-        let mut ranfloat: Vec<f64> = Vec::new();
+        let mut ranvec: Vec<Vec3> = Vec::new();
         for _i in 0..POINT_COUNT {
-            ranfloat.push(random_double());
+            ranvec.push(unit_vector(random_vec3_rng(-1.0, 1.0)));
         }
 
         let perm_x = perlin_generate_perm();
         let perm_y = perlin_generate_perm();
         let perm_z = perlin_generate_perm();
         Perlin {
-            ranfloat,
+            ranvec,
             perm_x,
             perm_y,
             perm_z,
         }
     }
     pub fn noise(&self, p: Point3) -> f64 {
-        let mut u = p.e0 - p.e0.floor();
-        let mut v = p.e1 - p.e1.floor();
-        let mut w = p.e2 - p.e2.floor();
-        u = u * u * (3.0 - 2.0 * u);
-        v = v * v * (3.0 - 2.0 * v);
-        w = w * w * (3.0 - 2.0 * w);
-
+        let u = p.e0 - p.e0.floor();
+        let v = p.e1 - p.e1.floor();
+        let w = p.e2 - p.e2.floor();
         let i = p.e0.floor() as i32;
         let j = p.e1.floor() as i32;
         let k = p.e2.floor() as i32;
-        let mut c: [[[f64; 2]; 2]; 2] = [[[0.0; 2]; 2]; 2];
+        let mut c: [[[Vec3; 2]; 2]; 2] = [[[Vec3::new(0.0, 0.0, 0.0); 2]; 2]; 2];
 
         for di in [0, 1] {
             for dj in [0, 1] {
                 for dk in [0, 1] {
-                    c[di][dj][dk] = self.ranfloat[(self.perm_x[((i as usize) + di) & 255]
+                    c[di][dj][dk] = self.ranvec[(self.perm_x[((i as usize) + di) & 255]
                         ^ self.perm_y[((j as usize) + dj) & 255]
                         ^ self.perm_z[((k as usize) + dk) & 255])
                         as usize];
@@ -68,15 +64,20 @@ pub fn permute(p: &mut [i32], n: i32) {
         p.swap(i as usize, target as usize);
     }
 }
-pub fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+pub fn trilinear_interp(c: [[[Vec3; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    let uu = u * u * (3.0 - 2.0 * u);
+    let vv = v * v * (3.0 - 2.0 * v);
+    let ww = w * w * (3.0 - 2.0 * w);
     let mut accum = 0.0;
+
     for i in [0, 1] {
         for j in [0, 1] {
             for k in [0, 1] {
-                accum += ((i as f64) * u + ((1 - i) as f64) * (1.0 - u))
-                    * ((j as f64) * v + ((1 - j) as f64) * (1.0 - v))
-                    * ((k as f64) * w + ((1 - k) as f64) * (1.0 - w))
-                    * c[i][j][k];
+                let weight_v = Vec3::new(u - (i as f64), v - (j as f64), w - (k as f64));
+                accum += ((i as f64) * uu + ((1 - i) as f64) * (1.0 - uu))
+                    * ((j as f64) * vv + ((1 - j) as f64) * (1.0 - vv))
+                    * ((k as f64) * ww + ((1 - k) as f64) * (1.0 - ww))
+                    * dot(c[i][j][k], weight_v);
             }
         }
     }
